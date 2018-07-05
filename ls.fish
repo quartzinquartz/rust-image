6bcce0166eee3b1b48e2503880ec418dc6cd0035
Derek Jenkins <derektjenkins@gmail.com>
2018-07-05 10:03:29 -0400

    makes ls replacement not fail on -A
    also adds headers via -h

--- ls.fish
+++ ls.fish
@@ -1,3 +1,8 @@
-function ls
-  exa -Fg --git --group-directories-first $argv
+function ls --description 'List contents of directory'
+	if test (echo $argv | sed '/-/!d')
+        and set _args (echo $argv | sed 'y/ /\n/;/^-/{y/A/a/;}')
+    else
+        set _args $argv
+    end
+    exa -Fgh --git --group-directories-first $_args
 end
